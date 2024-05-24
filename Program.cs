5679904779f6808968b2933bd47878fa427eb2a0
toandzpro <kimthoai15620@gmail.com>
2024-05-25 00:55:06 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Animal;
+
+public class baitap26
+{
+    public static void Main(string[] args)
+    {
+         Cat cat = new Cat("20kg", "1.5", "kitty");
+         cat.PrintInfo();
+    }
+}
